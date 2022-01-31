6540e92a5f2a9a0a4d44e5a577e96fb6d211017e
ann13-13 <75249486+ann13-13@users.noreply.github.com>
2022-01-31 15:51:05 +0300

    Add files via upload
    a

---
+++ YogaApp/YogaApp/TableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  TableViewCell.swift
+//  YogaApp
+//
+//  Created by Student on 26.01.2022.
+//
+
+import UIKit
+import WatchConnectivity
+
+class TableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var picture: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+        
+}
